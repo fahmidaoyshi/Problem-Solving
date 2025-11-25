ced6d062fdee207a0b494a0f1aff1af9798c2ed8
fahmidaoyshi77-art <fahmidaoyshi77@gmail.com>
2025-11-25 05:51:46 -0800

    Problem 3

--- index.js
+++ index.js
@@ -47,3 +47,48 @@ console.log(countVowels(["hello"]));
 console.log(countVowels(["Bangladesh"]));
 console.log(countVowels([12345]));      
 console.log(countVowels(["rhythm"]));
+
+ // Mark Sheet
+function evaluateResult(marks) {
+  if (!Array.isArray(marks)) {
+    return "Invalid Input";
+  }
+  let grades = [];
+  for (let data of marks) {
+    if (typeof data === "number") {
+      if (data > 0 && data <= 100) {
+        let marks = data;
+        if (marks >= 80) {
+          grades.push("A+");
+        } else if (marks >= 70) {
+          grades.push("A");
+        } else if (marks >= 60) {
+          grades.push("A-");
+        } else if (marks >= 50) {
+          grades.push("B");
+        } else if (marks >= 40) {
+          grades.push("C");
+        } else if (marks >= 33) {
+          grades.push("D");
+        } else if (marks < 33) {
+          grades.push("F");
+        }
+      }       
+     else if (data < 0) {
+        grades.push("Invalid Input");
+      } 
+     else if (data > 100) {
+        grades.push("Invalid Input");
+      }
+    } 
+  else {
+      grades.push("Invalid Input");
+    }
+  }
+  return grades;
+}
+
+console.log(evaluateResult([45, 67, 82, 95, "a", null])); 
+console.log(evaluateResult([90, 80, 70, 60, 50, 40]));   
+console.log(evaluateResult([30]));                     
+console.log(evaluateResult([100, 110, -5]));  
