026a47d8c2d02ccd748678eee4c3ef4ebe854a49
fahmidaoyshi77-art <fahmidaoyshi77@gmail.com>
2025-11-25 04:30:19 -0800

    problem 2

--- index.js
+++ index.js
@@ -17,3 +17,33 @@ console.log(fixWeightMachine([50, -10, 0, 75, "a", null]));
 console.log(fixWeightMachine("Not an Array"));                    
 console.log(fixWeightMachine([100, NaN, -5, 30, undefined]));     
 console.log(fixWeightMachine([]));                               
+
+// Check Vowels
+function countVowels(B) {
+  var t = typeof B;
+  if (t !== "string") {
+    var vowels = "aeiouAEIOU";
+    var count = 0;
+    for (let data of B) {
+      if (typeof data !== "string") {
+        return "Invalid Input";
+      }
+      for (let t of data) {
+        var check = vowels.includes(t);
+        if (check === true) {
+          count++;
+        } else {
+          count = count;
+        }
+      }
+    }
+  } else {
+    return "Invalid Input";
+  }
+  return count;
+}
+
+console.log(countVowels(["hello"]));
+console.log(countVowels(["Bangladesh"]));
+console.log(countVowels([12345]));      
+console.log(countVowels(["rhythm"]));
